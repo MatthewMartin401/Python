e20d857b4a823f90f8c922b573d56319abd6a353
matt <129974886+MatthewMartin401@users.noreply.github.com>
2024-05-22 20:39:40 +0100

    Update
    Added:
    - Turns.
    - Replay pop up window.
    - Lose / Win conditions.
    - HUD.
    - Grid outline.
    - COLN and ROWN Variables can be changed without negatively affecting the index detection.
    
    Improved:
    - Location detection, through the use of ceil.

--- Dots&Boxes.py
+++ Dots&Boxes.py
@@ -4,15 +4,20 @@ Created on Thu May 16 14:04:04 2024
 
 @author: user
 """
-#  Creator: Me
-#  Complete: False
+#  Coder: Me
+#  Complete: True
 
-import turtle, copy
+import tkinter.messagebox
+import turtle
+from math import ceil
 
 class Dots_and_Boxes:
     def __init__(self, t):
         self.t = t
-        t.screen.setworldcoordinates(urx=1000,ury=1000, llx=0, lly=0)
+        
+        t.screen.title("Dots and Boxes.")
+        t.screen.setworldcoordinates(urx=905,ury=1000, llx=0, lly=0)
+        t.screen.setup(width = 1000, height = 850)
         self.COLN, self.ROWN = 10, 10
         self.GAP = 100
         self.point_A = None
@@ -20,6 +25,7 @@ class Dots_and_Boxes:
         self.turn = True
         self.colors = {True: "red",
                        False: "blue"}
+        self.h = turtle.Turtle()
 
         self.grid = self.create_grid()
         self.valid_options = self.get_valid_lines()  # Interactable lines - 2 values. One for each dot. 2 dots stored in 2 lists.
@@ -28,13 +34,39 @@ class Dots_and_Boxes:
             False: [],
             True: []
         }
+        self.play = True
+
+    def reset(self):
+        self.t.clear()
+        self.t.color("black")
+        self.create_dots()
+        self.valid_options = self.get_valid_lines() 
+        self.User_scores = {
+            False: [],
+            True: []
+        }
+
+    def update_hud(self):
+        self.h.clear()
+        self.h.color("black")
+        self.h.up()
+        self.h.goto(0, 950)
+        self.h.write(f"{str(self.colors[True]).title()}: {len(self.User_scores[True])} {str(self.colors[False]).title()}: {len(self.User_scores[False])}", font=("Ariel", 20, "normal"))
     
+    def main(self):
+        self.t.clear()
+        self.update_hud()
+        self.create_dots()
+        turtle.tracer(2, 4)
+        self.t.screen.onclick(game.draw_line)
+        turtle.listen()
+        self.t.screen.mainloop()
+
     def correct_points(self): # Puts points in Ascending order.
         #  Sorts point A and point B order, so that it can be found in the array
         # print("Here", self.point_B[0])
         if (self.point_A[0] > self.point_B[0]) or (self.point_A[1] > self.point_B[1]):
             self.point_A, self.point_B = self.point_B, self.point_A
-
     
     def new_turn(self):  # Occurs at the end of turn.
         self.turn = not self.turn  # New turn
@@ -61,15 +93,20 @@ class Dots_and_Boxes:
                         lines.append([[x, y], [x1, y1]])
         return lines
     
-    def fill_box(self, location):
+    def get_winner(self):
+        return "Draw" if len(self.User_scores[True]) == len(self.User_scores[False]) else (str(self.colors[True]).title() if len(self.User_scores[True]) > len(self.User_scores[False]) else str(self.colors[False]).title())
+    
+    def fill_box(self, box):
         self.t.up()
-        self.t.goto(location[0][0])  # Start location
+        self.t.goto(box[0][0])  # Start location
         self.t.begin_fill()
-        for line in location:
+        for line in box:
             for dot in line:
                 # print("location: ", dot)
                 self.t.goto(dot)
-        self.t.goto
+        print(box[0][0])
+        self.t.goto(box[2][0])
+        self.t.goto(box[2][1])
         self.t.fillcolor(self.colors[self.turn])
         self.t.end_fill()
     
@@ -77,8 +114,8 @@ class Dots_and_Boxes:
         boxes = list()
         print(self.grid)
 
-        for i in range(0, self.COLN * self.ROWN - 10, 1):
-            if (i + 1) % 10 != 0:
+        for i in range(0, (self.COLN * self.ROWN) - self.ROWN, 1):
+            if (i + 1) % self.ROWN != 0:
                 print(f"{i} / {len(self.grid)-11}")
                 boxes.append([
                     [self.grid[i], self.grid[i + (self.ROWN)]], # Bottom
@@ -90,13 +127,37 @@ class Dots_and_Boxes:
         return boxes
 
     def get_dot_index(self, x, y):
-        # print(x, y)
-        x = round(x//100)  # Might be replaced with self.GAP
-        y = round(y//100)  # Might be replaced with self.GAP
-        # print(x, y)
-        index = int(f"{x}{y}")
-        print(index)
-        return index
+        print(x, y)
+        
+        # Round x up condition
+        print((int(x // self.GAP) * self.GAP) + (self.GAP / 2), (int(y // self.GAP) * self.GAP) + (self.GAP / 2))
+        if x >= (int(x // self.GAP) * self.GAP) + (self.GAP / 2):
+            x = (ceil(x / 100) * 100) // self.GAP
+            print("ceil: ", x)
+        else:#
+            x = int(x // self.GAP)  # Rounds down.
+        
+        # Round y up condition
+        if y >= (int(y // self.GAP) * self.GAP) + (self.GAP / 2):
+            y = (ceil(y / 100) * 100) // self.GAP
+            print("ceil: ", y)
+        else:
+            y = int(y // self.GAP)  # Rounds down.
+        print((int(x // self.GAP) * self.GAP))
+        
+       
+        
+        # index = x + (y % 9)
+
+        #  Works as long as its 10 x 10 dots.
+        # index = int(f"{x}{y}")
+
+        if (y + 1) <= self.ROWN:
+            index = (self.ROWN * x)+ y  # Number of rows multiplied by grid index. Plus y (y is the height on the grid.)
+            # print("coln * x: ", self.COLN * x, "y :", y)
+            # print("INDEX: ", index)
+
+            return index
         # t.goto(min(grid, lambda a: abs(a - (x, y))))
 
     def create_dots(self):
@@ -106,11 +167,45 @@ class Dots_and_Boxes:
             self.t.goto(l[0], l[1])
             self.t.dot()
             self.t.write(index)
+        self.t.up()
+        self.t.goto(-20, -20)
+        self.t.down()
+        for i in [[-20, -20], [((self.ROWN - 1)* self.GAP) + 20, -20], [((self.ROWN - 1) * self.GAP) + 20, ((self.COLN - 1) * self.GAP) + 20], [-20, ((self.COLN - 1) * self.GAP) + 20], [-20, -20]]:
+            self.t.goto(i)    
+        
+        self.t.up()
+        
 
         #  Changes from black to user turn colors.
         t.color(self.colors[self.turn])
             # if (index + 1) % 10 == 0 and index != 0:
             #     t.write(int((index + 1) / 10))
+
+    def release_input(event):
+        # print(event)
+        # print(event.state)
+        # Mouse left-key.
+        if event.num == 1:
+            # print(event)
+            return [event.x, event.y]
+        
+    def set_Point(self, x, y):
+        # print(x, y)
+        index = self.get_dot_index(x, y)  # Gets the index.
+        if index != None:
+            if index <= len(self.grid) and index >= 0:
+                # print(f"{index} found in grid.")
+
+                # Sets the location. Alternate method: Use just the index, instead of the value.
+                if self.point_B != None and self.point_A != None:
+                    self.point_A, self.point_B = None, None
+                if self.point_A == None:
+                    self.point_A = self.grid[index]
+                    # print(f"set_pointA: {self.point_A}")
+                elif self.point_B == None:
+                    self.point_B = self.grid[index]
+                    # print(f"set_pointB: {self.point_B}")
+            # print("HERE:", point_A, point_B)
 
     def draw_all_Lines(self):
         #  SHOWS LINE GENERATION
@@ -126,45 +221,20 @@ class Dots_and_Boxes:
             self.t.goto(i[1])
             print(i)
 
-    def release_input(event):
-        # print(event)
-        # print(event.state)
-        # Mouse left-key.
-        if event.num == 1:
-            # print(event)
-            return [event.x, event.y]
-        
-    def set_Point(self, x, y):
-        # print(x, y)
-        index = self.get_dot_index(x, y)  # Gets the index.
-        if index <= len(self.grid) and index >= 0:
-            print(f"{index} found in grid.")
-
-            # Sets the location. Alternate method: Use just the index, instead of the value.
-            if self.point_B != None and self.point_A != None:
-                self.point_A, self.point_B = None, None
-            if self.point_A == None:
-                self.point_A = self.grid[index]
-                # print(f"set_pointA: {self.point_A}")
-            elif self.point_B == None:
-                self.point_B = self.grid[index]
-                # print(f"set_pointB: {self.point_B}")
-        # print("HERE:", point_A, point_B)
-
     def draw_line(self, x, y):
         # x, y = event.x, event.y
         self.set_Point(x, y)
-        self.t.color
+        score = len(self.User_scores[self.turn])
         
         if self.point_A != None and self.point_B != None:  #  Both locations must be set.
             self.correct_points()  # Puts points in Ascending order.
             if [self.point_A, self.point_B] in self.valid_options:  # Must be a valid location. Draw lines.
                 # print(True)
-                t.up()
-                t.goto(self.point_A)
-                t.down()
-                t.goto(self.point_B)
-                t.up()
+                self.t.up()
+                self.t.goto(self.point_A)
+                self.t.down()
+                self.t.goto(self.point_B)
+                self.t.up()
 
                 # print(self.boxes)
                 # print([self.point_A, self.point_B])
@@ -179,123 +249,28 @@ class Dots_and_Boxes:
                         # print(c)
                         # print("check", [a not in self.valid_options for a in x])
                         if all([a not in self.valid_options for a in x]):
-                            print("hi")
                             self.fill_box(x)
                             self.User_scores[self.turn].append(x)
                     
-                    print(self.colors[self.turn], len(self.User_scores[self.turn]))
+                    # print(self.colors[self.turn], len(self.User_scores[self.turn]))
 
-                self.new_turn()
-                # print("Done")
-                # for x in self.boxes:  # List of all lines within the box.
-                    # print([self.point_A, self.point_B] in x)
-                    # print("-", True in ([self.point_A, self.point_B] in x))
-                    #  Checks if new lines are apart of a box, and whether all values have been removed from valid lines.
-
-                    # apart_of_box = list([self.point_A, self.point_B] in i for i in x)
-                    # if all(apart_of_box) and True in apart_of_box:  # Line apart of box?
-                        # print("HERE - 1: ", all([val not in self.valid_options] for val in x))
-                        
-                        # print("hi")
-                        # print(self.valid_options)
-                        # for val in x:
-                                # print(val in self.valid_options)  # Returns false, as values are stored in different structures.
-
-                        # if all(list([val not in self.valid_options] for val in x)):
-                        #     print("BOXES: ", x)
-                        #     print("ALL LINES IN BOX: ", list([val not in self.valid_options] for val in x))
-                        #     print("found")
+                #  Info
+                print(len(self.valid_options))
+                self.update_hud()
+                if len(self.User_scores[self.turn]) == score:  # Only starts new turn, if user score didnt increase.
+                    self.new_turn()
+                    
                 
+        #  Checks if the game is over.
+        if len(self.valid_options) == 0:
+            self.play = tkinter.messagebox.askyesno(message = f"Winner: {self.get_winner()}. Would you like to play again?", title="Game Over")
+            if self.play == True:
+                self.reset()
             else:
-                print([self.point_A, self.point_B])
-                print(self.valid_options)
-
-
-#  Check whether dots are near
-
-    
-
-
-#  Who filled in the last line.
-
-#  Adds box to user score.
+                turtle.bye()
 
 t = turtle.Turtle()
-# background = turtle.Screen()
-# background = t.screen.getcanvas()
 turtle.tracer(0, 0)
 
 game = Dots_and_Boxes(t)
-game.create_grid()
-game.create_dots()
-turtle.tracer(2, 4)
-# for v in game.boxes:
-#     game.fill_box(v)
-# game.fill_box(v)
-# print(v)
-
-# box = game.get_all_boxes()
-# turtle.tracer(1, 2)
-# for l in box:
-#     t.up()
-#     t.end_fill()
-#     print(l)
-#     t.goto(l[0])
-#     for v in l:
-#         t.begin_fill()
-#         t.goto(v)
-#     t.goto(l[0])
-#     t.goto(l[1])
-#     t.fillcolor(game.colors[game.turn])
-
-game.t.screen.onclick(game.draw_line)
-turtle.listen()
-t.screen.mainloop()
-
-# lines = game.get_valid_lines(grid)
-# valid_lines = copy.deepcopy(lines)
-# game.create_dots(t)
-# print(grid)
-# t.screen.onclick(game.draw_line)
-
-
-
-
-
-    # Works, but causes an issue with x and y coords.
-    # Couldnt get ondrag to work or onrelease to give the correct coordinates.
-# t.screen.getcanvas().bind("<ButtonRelease>", draw_line)
-# cavset.bind("<Button-1>", f)
-# t.screen.onclick(drawLines)  # Works
-# t.screen.onclick(set_Point)  # Works
-# t.screen.onkeyrelease(draw_line, "1")
-# t.ondrag(draw_line)
-
-# background.onkeyrelease(f)
-# background.onkeyrelease(f, key=1)
-
-# cavset.bind("<Button-1>", f)
-
-
-# t.onrelease(get_dot_index, 1)  # Doesnt work
-# t.ondrag(t.goto, 1)  # Doesnt work
-
-# t.ondrag(f)
-
-
-
-#  Drawing lines:
-# for x in range(len(col)):
-#     gap = 100 * x
-#     for y in [300]:
-#         t.up()
-#         t.goto(gap, 0)
-#         t.down()
-#         t.goto(gap, y)
-
-
-# t.speed(5)
-# t.up()
-# t.goto(0, 0)
-# t.down()
-# t.goto(-500, 500)
+game.main()
